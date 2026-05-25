16b19a3a5f25d50fd224b97cc047e657c7e966a4
penicili <bagaskoro55@gmail.com>
2026-05-25 14:10:13 +0700

    dashboard udh

--- src/pages/api/auth/[...all].ts
+++
@@ -1,5 +0,0 @@
-export const prerender = false;
-import { auth } from '../../../lib/auth';
-import type { APIRoute } from 'astro';
-
-export const ALL: APIRoute = ({ request }) => auth.handler(request);
